7a3c95e5e2db3555664c0e9737032768df4940b2
Maciej Brencz <maciej.brencz@gmail.com>
2015-01-21 22:37:09 +0100

    Add unit tests for Phantomas.format_args

---
+++ phantomas/test/test_format_args.py
@@ -0,0 +1,15 @@
+"""
+Set of unit tests for helper functions
+"""
+import unittest
+
+from phantomas import Phantomas
+
+
+class FormatArgsTestClass(unittest.TestCase):
+    def test_format_args(self):
+        self.assertEqual(Phantomas.format_args(dict()), [])
+        self.assertEqual(Phantomas.format_args(dict(foo="bar")), ['--foo=bar'])
+        self.assertEqual(Phantomas.format_args(dict(check=True)), ['--check'])
+        self.assertEqual(Phantomas.format_args(
+            dict(list=['foo', 'bar', 123])), ['--list=foo,bar,123'])
