63b1b1bd3fc97feb460beb6ae509bfb5cccf04f5
Maciej Brencz <maciej.brencz@gmail.com>
2016-07-01 23:18:21 +0200

    tag.sh

--- tag.sh
+++ tag.sh
@@ -3,7 +3,7 @@ VERSION=`grep -r '__version__' phantomas/__init__.py | awk -F\' '{ print $2 }'`
 
 echo "Tagging v${VERSION}..."
 
-git tag -a v${VERSION}
+git tag -a v${VERSION} -m "v${VERSION}"
 git push origin v${VERSION}
 
 echo "Done"
